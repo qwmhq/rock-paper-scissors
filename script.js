daa3254aed5dd82c0f10c7f228e16666f2ee2ba5
Quwam Ogunlaja <quwam32@gmail.com>
2021-09-15 19:08:22 +0100

    Created function computerPlay() in script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay() {
+    let options = ['Rock', 'Paper', 'Scissors'];
+    let randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+
+function singleRound() {
+    
+}
+
+function game() {
+
+}
