2627962d124804792d73396a68c61159e3730e01
Quwam Ogunlaja <quwam32@gmail.com>
2021-09-15 19:34:50 +0100

    Added function singleRound() and helper function capitalize()

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ function computerPlay() {
     return options[randomIndex];
 }
 
-function singleRound() {
-    
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1, str.length)
+}
+
+function singleRound(playerSelection, computerSelection) {
+
+    playerSelection = capitalize(playerSelection);
+
+    if ( (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') )  {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === computerSelection) {
+        return 'You draw this round!';
+    } else return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
 
 function game() {
